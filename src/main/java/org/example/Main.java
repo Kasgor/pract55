e09b4dd6181e87a65d9b4d4ccc0779c4588013da
Kasgor <shadow.crst@gmail.com>
2024-06-17 12:42:56 +0300

    Initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,20 @@
 package org.example;
 
+/**
+ * The main class of the application.
+ */
 public class Main {
-    public static void main(String[] args) {
+    // Private constructor to prevent instantiation
+    private Main() {
+        throw new UnsupportedOperationException("Utility class");
+    }
+
+    /**
+     * The main method of the application.
+     *
+     * @param args the input arguments
+     */
+    public static void main(final String[] args) {
         System.out.println("Hello world!");
     }
 }
